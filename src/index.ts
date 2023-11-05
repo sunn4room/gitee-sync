af5de94c62c11cac16efe3a14581ff32cc10fac6
sunn4ovo <sunn4ovo@163.com>
2023-11-05 20:50:37 +0800

    v1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { info, warning, setFailed, getInput } from "@actions/core"
+import { info, warning, setFailed, getInput, getMultilineInput } from "@actions/core"
 import { launch } from "puppeteer"
 import { downloadBrowser } from "puppeteer/lib/cjs/puppeteer/node/install.js"
 
@@ -6,7 +6,7 @@ import { downloadBrowser } from "puppeteer/lib/cjs/puppeteer/node/install.js"
 
   const username = getInput("username", { required: true })
   const password = getInput("password", { required: true })
-  const repositories = getInput("repositories", { required: true })
+  const repositories = getMultilineInput("repositories", { required: true })
 
   await downloadBrowser().catch(() => {
     throw new Error("cannot download browser")
@@ -14,14 +14,14 @@ import { downloadBrowser } from "puppeteer/lib/cjs/puppeteer/node/install.js"
 
   const browser = await launch({
     headless: "new",
-    args: ['--lang=zh-CN'],
+    args: ["--lang=zh-CN"],
   }).catch(() => {
     throw new Error("cannot launch browser")
   })
 
-  const goto = async (url: string) => {
+  const goto = async (url: string, until: any = "domcontentloaded") => {
     const page = await browser.newPage()
-    await page.goto(url)
+    await page.goto(url, { waitUntil: until }).catch(() => {})
     return page
   }
 
@@ -38,7 +38,7 @@ import { downloadBrowser } from "puppeteer/lib/cjs/puppeteer/node/install.js"
     await login_page.type(username_selector, username)
     await login_page.type(password_selector, password)
     await Promise.all([
-      login_page.waitForNavigation(),
+      login_page.waitForNavigation({ waitUntil: "domcontentloaded" }),
       login_page.click(login_btn_selector)
     ])
     await login_page.close()
@@ -55,7 +55,7 @@ import { downloadBrowser } from "puppeteer/lib/cjs/puppeteer/node/install.js"
       await repo_page.click(sync_btn_selector)
       await repo_page.waitForSelector(confirm_btn_selector)
       await Promise.all([
-        repo_page.waitForNavigation(),
+        repo_page.waitForNavigation({ waitUntil: "domcontentloaded" }),
         repo_page.evaluateHandle(() => {
           const confirm = document.querySelector("#modal-sync-from-github > .actions > .orange.ok") as any
           confirm.click()
@@ -65,14 +65,59 @@ import { downloadBrowser } from "puppeteer/lib/cjs/puppeteer/node/install.js"
       info(repo)
     } catch {
       warning(repo)
+      throw new Error("Please read the warning message")
+    }
+  }
+
+  class LimitPromise {
+    private limit: number
+    private count: number
+    private taskQueue: any[]
+
+    constructor(limit: number) {
+      this.limit = limit
+      this.count = 0
+      this.taskQueue = []
+    }
+
+    private createTask(
+      asyncFn: Function,
+      args: any[],
+      resolve: (value: void) => void,
+      reject: (reason?: any) => void
+    ) {
+      return () => {
+        asyncFn(...args)
+          .then(resolve)
+          .catch(reject)
+          .finally(() => {
+            this.count--
+            if (this.taskQueue.length) {
+              let task = this.taskQueue.shift()
+              task()
+            }
+          })
+
+        this.count++
+      }
+    }
+
+    public call(asyncFn: Function, ...args: any[]) {
+      return new Promise<void>((resolve, reject) => {
+        const task = this.createTask(asyncFn, args, resolve, reject)
+        if (this.count >= this.limit) {
+          this.taskQueue.push(task)
+        } else {
+          task()
+        }
+      })
     }
   }
 
   const promises: Promise<void>[] = []
-  repositories.split("\n").forEach((repo) => {
-    if (repo.indexOf("/") >= 0) {
-      promises.push(sync(repo))
-    }
+  const promise_limiter = new LimitPromise(5)
+  repositories.forEach(async (repo) => {
+    promises.push(promise_limiter.call(sync, repo))
   })
   await Promise.all(promises)
 
